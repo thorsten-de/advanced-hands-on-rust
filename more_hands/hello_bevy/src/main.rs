d35444c044a09b23d974c1aec3a22deddf1fb109
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-09-22 18:09:28 +0200

    Display a graphic with Bevy

--- more_hands/hello_bevy/src/main.rs
+++ more_hands/hello_bevy/src/main.rs
@@ -1,5 +1,20 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .run();
+}
+
+#[derive(Component)]
+struct Dragon;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2d::default());
+    let dragon_image = asset_server.load("dragon.png");
+
+    commands
+        .spawn(Sprite::from_image(dragon_image))
+        .insert(Dragon);
 }
