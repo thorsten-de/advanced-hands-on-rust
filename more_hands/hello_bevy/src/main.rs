910c3fa407eb3b7e3bcc91bb181e303689f9ae79
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-09-22 18:20:59 +0200

    Moving entities with Bevy

--- more_hands/hello_bevy/src/main.rs
+++ more_hands/hello_bevy/src/main.rs
@@ -1,9 +1,10 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, transform};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, movement)
         .run();
 }
 
@@ -17,4 +18,25 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands
         .spawn(Sprite::from_image(dragon_image))
         .insert(Dragon);
+}
+
+fn movement(
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut dragon_query: Query<&mut Transform, With<Dragon>>,
+) {
+    let delta = if keyboard.pressed(KeyCode::ArrowLeft) {
+        Vec2::new(-1.0, 0.0)
+    } else if keyboard.pressed(KeyCode::ArrowRight) {
+        Vec2::new(1.0, 0.0)
+    } else if keyboard.pressed(KeyCode::ArrowDown) {
+        Vec2::new(0.0, -1.0)
+    } else if keyboard.pressed(KeyCode::ArrowUp) {
+        Vec2::new(0.0, 1.0)
+    } else {
+        Vec2::ZERO
+    };
+
+    dragon_query.iter_mut().for_each(|mut transform| {
+        transform.translation += delta.extend(0.0);
+    });
 }
