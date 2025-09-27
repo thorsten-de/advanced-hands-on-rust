1d8f8ccb5ebe7209ecf7b71794b2ee620d5b964d
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-09-27 18:33:01 +0200

    Remove example function

--- more_hands/my_library/src/lib.rs
+++ more_hands/my_library/src/lib.rs
@@ -1,16 +1,10 @@
 pub use rand;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = 2 + 2;
         assert_eq!(result, 4);
     }
 }
