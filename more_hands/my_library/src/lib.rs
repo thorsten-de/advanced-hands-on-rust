264c55859574c40302eacc0677ed9b1c72150af2
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-09-27 19:02:41 +0200

    Remove example test

--- more_hands/my_library/src/lib.rs
+++ more_hands/my_library/src/lib.rs
@@ -2,12 +2,3 @@ pub use rand;
 
 mod random;
 pub use random::*;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
