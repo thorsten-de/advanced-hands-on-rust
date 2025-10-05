9a9fc890511fb75d15835e0a54c767775ad16708
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-10-05 18:34:16 +0200

    Add module level documentation for my_library

--- more_hands/my_library/src/lib.rs
+++ more_hands/my_library/src/lib.rs
@@ -1,3 +1,27 @@
+//! `my_library` provides a suite of helpers to create games with Bevy.
+//!
+//! Whats included?
+//! ---------------
+//!
+//! `my_library` includes:
+//!
+//! - Random number generation facilities
+//!
+//!
+//! Feature flags
+//! -------------
+//!
+//! The following feature flags are supported: `xorshift`, `pcg`, `locking`
+//!
+//! ### Random number generation
+//!
+//! - The `locking` feature enables interior mutability inside [`RandomNumberGenerator`],
+//!   allowing it to be used as a resource (`Res<RandomNumberGenerator`) rather than
+//!   requiring mutability (`ResMut<RandomNumberGenerator`)
+//! - You can control which random number generation algorithm is used by specifying one of:
+//!     - `xorshift` to use the XorShift algorithm
+//!     - `pcg` to use the PCG algorithm
+
 pub use rand;
 
 #[cfg(not(feature = "locking"))]
