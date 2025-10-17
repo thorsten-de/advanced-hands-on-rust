84cc7981fd85bed16839289c4300f198f1846cfc
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-10-17 15:15:21 +0200

    Adding frame-based animation
    - animates flappy
    - define animations module

--- bevy_games/flappy_dragon/src/main.rs
+++ bevy_games/flappy_dragon/src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ecs::entity::FromEntitySetIterator, prelude::*};
 use my_library::*;
 
 #[derive(Component)]
@@ -28,7 +28,7 @@ fn main() -> anyhow::Result<()> {
 
     add_phase!(app, GamePhase, GamePhase::Flapping,
         start => [ setup ],
-        run => [ gravity, flap, clamp, move_walls, hit_wall ],
+        run => [ gravity, flap, clamp, move_walls, hit_wall, cycle_animations ],
         exit => [ cleanup::<FlappyElement> ]
     );
 
@@ -52,7 +52,42 @@ fn main() -> anyhow::Result<()> {
             .add_image("dragon", "flappy_dragon.png")?
             .add_image("wall", "wall.png")?
             .add_sound("flap", "dragonflap.ogg")?
-            .add_sound("crash", "crash.ogg")?,
+            .add_sound("crash", "crash.ogg")?
+            .add_sprite_sheet("flappy", "flappy_sprite_sheet.png", 62.0, 65.0, 4, 1)?,
+    )
+    .insert_resource(
+        Animations::new()
+            .with_animation(
+                "Straight and Level",
+                PerFrameAnimation::new(vec![
+                    AnimationFrame::new(2, 500, vec![AnimationOption::NextFrame]),
+                    AnimationFrame::new(3, 500, vec![AnimationOption::GoToFrame(0)]),
+                ]),
+            )
+            .with_animation(
+                "Flapping",
+                PerFrameAnimation::new(vec![
+                    AnimationFrame::new(
+                        0,
+                        66,
+                        vec![
+                            AnimationOption::NextFrame,
+                            AnimationOption::PlaySound("flap".to_string()),
+                        ],
+                    ),
+                    AnimationFrame::new(1, 66, vec![AnimationOption::NextFrame]),
+                    AnimationFrame::new(2, 66, vec![AnimationOption::NextFrame]),
+                    AnimationFrame::new(3, 66, vec![AnimationOption::NextFrame]),
+                    AnimationFrame::new(2, 66, vec![AnimationOption::NextFrame]),
+                    AnimationFrame::new(
+                        1,
+                        66,
+                        vec![AnimationOption::SwitchToAnimation(
+                            "Straight and Level".to_string(),
+                        )],
+                    ),
+                ]),
+            ),
     )
     .run();
 
@@ -67,17 +102,35 @@ fn setup(
 ) {
     commands.spawn(Camera2d::default()).insert(FlappyElement); //(9)
 
-    spawn_image!(
+    spawn_animated_sprite!(
         assets,
         commands,
-        "dragon",
+        "flappy",
         -490.0,
         0.0,
-        1.0,
-        &loaded_assets,
+        10.0,
+        "Straight and Level",
         Flappy { gravity: 0.0 },
         FlappyElement
     );
+    /*
+    let Some((img, atlas)) = assets.get_atlas_handle("flappy") else {
+        panic!()
+    };
+    commands.spawn((
+        Sprite::from_atlas_image(
+            img.clone(),
+            TextureAtlas {
+                layout: atlas.clone(),
+                index: 0,
+            },
+        ),
+        Transform::from_xyz(-490.0, 0.0, 1.0),
+        AnimationCycle::new("Straight and Level"),
+        Flappy { gravity: 0.0 },
+        FlappyElement,
+    ));
+    */
 
     build_wall(&mut commands, &assets, &loaded_assets, rng.range(-5..5)); //(12)
 }
@@ -115,17 +168,11 @@ fn gravity(mut query: Query<(&mut Flappy, &mut Transform)>) {
     }
 }
 
-fn flap(
-    keyboard: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Flappy>,
-    assets: Res<AssetStore>,
-    loaded_assets: Res<LoadedAssets>,
-    mut commands: Commands,
-) {
+fn flap(keyboard: Res<ButtonInput<KeyCode>>, mut query: Query<(&mut Flappy, &mut AnimationCycle)>) {
     if keyboard.pressed(KeyCode::Space) {
-        if let Ok(mut flappy) = query.single_mut() {
-            flappy.gravity = -5.0; //(19)
-            assets.play("flap", &mut commands, &loaded_assets);
+        if let Ok((mut flappy, mut animation)) = query.single_mut() {
+            flappy.gravity = -5.0;
+            animation.switch("Flapping");
         }
     }
 }
--- bevy_games/my_library/src/bevy_framework/bevy_animation.rs
+++ bevy_games/my_library/src/bevy_framework/bevy_animation.rs
@@ -17,6 +17,7 @@ pub enum AnimationOption {
     PlaySound(String),
 }
 
+/// Defines a frame that is part of the animated sequence
 pub struct AnimationFrame {
     /// The index of the SpriteSheet frame to display, from 0 to the number
     /// of images in the sheet.
@@ -65,5 +66,111 @@ impl Animations {
     pub fn with_animation<S: ToString>(mut self, tag: S, animation: PerFrameAnimation) -> Self {
         self.0.insert(tag.to_string(), animation);
         self
+    }
+}
+
+/// A component to attach the animation state machine to the animated entity
+#[derive(Component)]
+pub struct AnimationCycle {
+    /// the tag to refer to this animation sequence
+    animation_tag: String,
+
+    /// the frame currently executed in this animation
+    current_frame: usize,
+
+    /// The time elapsed since animation was rendered the last time. This keeps
+    /// the timer state independently for each executing animation.
+    timer: u128,
+}
+
+impl AnimationCycle {
+    /// Creates a new state machine for a given tag starting at time/frame 0
+    pub fn new<S: ToString>(tag: S) -> Self {
+        Self {
+            animation_tag: tag.to_string(),
+            current_frame: 0,
+            timer: 0,
+        }
+    }
+
+    /// Switches an already running animation to a *different* animation sequence
+    pub fn switch<S: ToString>(&mut self, new: S) {
+        let new = new.to_string();
+        if new != self.animation_tag {
+            self.animation_tag = new;
+            self.current_frame = 0;
+            self.timer = 0;
+        }
+    }
+}
+
+/// System that animates frame sequences by using animation data
+pub fn cycle_animations(
+    animations: Res<Animations>,
+    mut animated: Query<(&mut AnimationCycle, &mut Sprite)>, // mutable access to all entities with both AnimationCycle and Sprite components
+    time: Res<Time>,
+    assets: Res<crate::AssetStore>,
+    mut commands: Commands,
+    loaded_assets: Res<crate::LoadedAssets>,
+) {
+    let ms_since_last_call = time.delta().as_millis();
+
+    animated.iter_mut().for_each(|(mut animation, mut sprite)| {
+        animation.timer += ms_since_last_call;
+
+        if let Some(cycle) = animations.0.get(&animation.animation_tag) {
+            let current_frame = &cycle.frames[animation.current_frame];
+
+            if animation.timer > current_frame.delay_ms {
+                animation.timer = 0;
+                for action in current_frame.action.iter() {
+                    match action {
+                        AnimationOption::None => {}
+                        AnimationOption::NextFrame => {
+                            animation.current_frame += 1;
+                        }
+                        AnimationOption::SwitchToAnimation(other) => {
+                            animation.switch(other);
+                        }
+                        AnimationOption::GoToFrame(frame) => {
+                            animation.current_frame = *frame;
+                        }
+                        AnimationOption::PlaySound(tag) => {
+                            assets.play(tag, &mut commands, &loaded_assets);
+                        }
+                    }
+
+                    if let Some(texture_atlas) = &mut sprite.texture_atlas {
+                        texture_atlas.index = cycle.frames[animation.current_frame].sprite_index;
+                    }
+                }
+            }
+        } else {
+            log::warn!("Animation Cycle [{}] not found!", animation.animation_tag);
+        }
+    });
+}
+
+/// Spawns an animated sprite
+#[macro_export]
+macro_rules! spawn_animated_sprite {
+    ($assets:expr, $commands:expr, $index:expr, $x:expr, $y:expr, $z:expr, $animation_name:expr, $($component:expr), *) =>
+    {
+        let Some((img, atlas)) = $assets.get_atlas_handle($index) else {
+            panic!()
+        };
+        $commands.spawn(
+            (Sprite::from_atlas_image(
+                img.clone(),
+                TextureAtlas {
+                    layout: atlas.clone(),
+                    index: 0,
+                }),
+            Transform::from_xyz($x, $y, $z),
+            AnimationCycle::new($animation_name),
+            ))
+            $(
+                .insert($component)
+            )*;
     }
 }
