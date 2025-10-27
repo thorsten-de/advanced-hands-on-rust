e363c90570f4f48f0a82a7869b2524630da11161
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-10-27 19:47:16 +0100

    Rotate flappy to look at end_frame position

--- bevy_games/flappy_dragon/src/main.rs
+++ bevy_games/flappy_dragon/src/main.rs
@@ -26,7 +26,8 @@ fn main() -> anyhow::Result<()> {
     add_phase!(app, GamePhase, GamePhase::Flapping,
         start => [ setup ],
         run => [ flap, clamp, move_walls, hit_wall, cycle_animations, continual_parallax,
-                 physics_clock, sum_impulses, apply_gravity, apply_velocity, check_collisions::<Flappy, Obstacle>],
+                 physics_clock, sum_impulses, apply_gravity, apply_velocity,
+                 check_collisions::<Flappy, Obstacle>, rotate],
         exit => [ cleanup::<FlappyElement> ]
     );
 
@@ -314,4 +315,17 @@ fn hit_wall(
         assets.play("crash", &mut commands, &loaded_assets);
         let _ = state.set(GamePhase::GameOver);
     }
+}
+
+fn rotate(mut physics_position: Query<(&PhysicsPosition, &mut Transform), With<Flappy>>) {
+    physics_position
+        .iter_mut()
+        .for_each(|(position, mut transform)| {
+            if position.start_frame != position.end_frame {
+                let start = position.start_frame;
+                let end = position.end_frame;
+                let angle = end.angle_to(start) * 10.0;
+                transform.rotation = Quat::from_rotation_z(angle);
+            }
+        });
 }
