67f129e4a4fbd0b3ec6a208b5869c23491c61d5f
Thorsten Deinert <thorsten.deinert@udo.edu>
2025-11-05 09:48:19 +0100

    Build a server
    - accepts POST /submit-score

--- bevy_games/highscore_server/src/main.rs
+++ bevy_games/highscore_server/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json, Router, routing::post};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/submit-score", post(submit_score));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3030")
+        .await
+        .unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(serde::Serialize, serde::Deserialize, Debug)]
+struct HighScoreEntry {
+    name: String,
+    score: u32,
+}
+
+async fn submit_score(high_score: Json<HighScoreEntry>) {
+    println!("Received high score {:?}", high_score);
 }
